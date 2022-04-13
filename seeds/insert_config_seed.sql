f311169501fdd5877106c97b8ec204c813636665
Filipe Batista <filipe.x@live.com>
2022-04-13 18:01:11 -0300

    finalização

--- seeds/insert_config_seed.sql
+++ seeds/insert_config_seed.sql
@@ -1,3 +1,3 @@
 INSERT INTO config(start_date, end_date, admin_user, admin_password, admin_name)
     VALUES
-        ('2022-03-01 06:00', '2022-03-30 18:00', 'admin', 'ascorsan@2022', 'Administrador Votação Ascorsan');
+        ('2022-04-18 06:00', '2022-04-19 18:00', 'admin', 'ascorsan@2022', 'Administrador Votação Ascorsan');
